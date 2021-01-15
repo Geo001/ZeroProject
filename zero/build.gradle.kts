99921b03934152ac5498b60d10841781322305bc
Geo001 <geovannaolivsantos@gmail.com>
2021-01-15 16:48:20 -0300

    project added

---
+++ zero/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.4.2"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.spring") version "1.4.21"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.mapstruct:mapstruct:1.4.0.CR1")
+	//kapt("org.mapstruct:mapstruct-processor:1.4.0.CR1")
+	testImplementation("org.testcontainers:testcontainers:1.14.3")
+	testImplementation("org.testcontainers:junit-jupiter:1.12.3")
+	testImplementation("org.testcontainers:mongodb:1.14.3")
+	testImplementation("com.github.tomakehurst:wiremock-jre8:2.27.2")
+	testImplementation("io.mockk:mockk:1.9.3")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ zero/src/main/kotlin/com/example/zero/ZeroApplication.kt
@@ -0,0 +1,11 @@
+package com.example.zero
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZeroApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZeroApplication>(*args)
+}
