0e740385b2b1f3948eb33789fa3613dc2ed22247
Geo001 <geovannaolivsantos@gmail.com>
2021-01-15 18:23:44 -0300

    kot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
 	id("org.springframework.boot") version "2.4.2"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
+	kotlin("kapt") version "1.3.61"
 }
 
 group = "com.example"
@@ -22,7 +24,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.mapstruct:mapstruct:1.4.0.CR1")
-	//kapt("org.mapstruct:mapstruct-processor:1.4.0.CR1")
+	kapt("org.mapstruct:mapstruct-processor:1.4.0.CR1")
 	testImplementation("org.testcontainers:testcontainers:1.14.3")
 	testImplementation("org.testcontainers:junit-jupiter:1.12.3")
 	testImplementation("org.testcontainers:mongodb:1.14.3")
